e7c99a4d38397d84b12221a3e11208ebcde5c736
Inaros <vkevinas99@gmail.com>
2022-10-25 12:17:51 +0300

    -"pacientas.py": added pasirinkimas 5 -"paciento_info2.py"added class: Ligonine

--- pacientas.py
+++ pacientas.py
@@ -1,55 +1,66 @@
 from sqlalchemy.orm import sessionmaker
-from paciento_info2 import engine, Pacientas, Kvepavimas, Kraujotaka, Neurologija
+from paciento_info2 import engine, Pacientas, Kvepavimas, Kraujotaka, Neurologija, Ligonine
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
 while True:
-    pasirinkimas = int(input("Įvesti pacienta - 1 \nĮvesti kvepavima - 2 \nĮvesti kraujavima - 3 \nĮvesti neurologija - 4 \nPeržiūrėti paciento duomenys - 7 \nIšeit - 0:"))
+    pasirinkimas = int(input("Įvesti pacienta - 1 \nĮvesti kvepavima - 2 \nĮvesti kraujavima - 3 \nĮvesti neurologija - 4 \nĮvesti ligoninė - 5 \nPeržiūrėti paciento duomenys - 7 \nIšeit - 0:"))
     if pasirinkimas == 1:
-            vardas = input("Įveskite varda: ")
-            pavarde = input("Įveskite pavardė: ")
-            asmens_kodas = int(input("Įveskite asmens koda: "))
-            pacientas = Pacientas(vardas=vardas, pavarde=pavarde, asmens_kodas=asmens_kodas)
-            session.add(pacientas)
-            session.commit()
+        vardas = input("Įveskite varda: ")
+        pavarde = input("Įveskite pavardė: ")
+        asmens_kodas = int(input("Įveskite asmens koda: "))
+        pacientas = Pacientas(vardas=vardas, pavarde=pavarde, asmens_kodas=asmens_kodas)
+        session.add(pacientas)
+        session.commit()
     if pasirinkimas == 2:
-            spo2 = input("Įveskite SpO2: ")
-            kd = input("Įveskite kvėpavimo dažni: ")
-            priskirtas_pacientas_id = int(input("Įveskite paciento ID: "))
-            kvepavimas = Kvepavimas(spo2=spo2, kd=kd, priskirtas_pacientas_id=priskirtas_pacientas_id)
-            session.add(kvepavimas)
-            session.commit()
+        spo2 = input("Įveskite SpO2: ")
+        kd = input("Įveskite kvėpavimo dažni: ")
+        priskirtas_pacientas_id = int(input("Įveskite paciento ID: "))
+        kvepavimas = Kvepavimas(spo2=spo2, kd=kd, priskirtas_pacientas_id=priskirtas_pacientas_id)
+        session.add(kvepavimas)
+        session.commit()
     if pasirinkimas == 3:
-            pulsas = input("Įveskite ŠSD: ")
-            aks = input("Įveskite AKS: ")
-            ekg = input("EKG: ")
-            priskirtas_pacientas_id = int(input("Įveskite paciento ID: "))
-            kraujo = Kraujotaka(pulsas=pulsas, aks=aks, ekg=ekg, priskirtas_pacientas_id=priskirtas_pacientas_id)
-            session.add(kraujo)
-            session.commit()
+        pulsas = input("Įveskite ŠSD: ")
+        aks = input("Įveskite AKS: ")
+        ekg = input("EKG: ")
+        priskirtas_pacientas_id = int(input("Įveskite paciento ID: "))
+        kraujo = Kraujotaka(pulsas=pulsas, aks=aks, ekg=ekg, priskirtas_pacientas_id=priskirtas_pacientas_id)
+        session.add(kraujo)
+        session.commit()
     if pasirinkimas == 4:
-            gks = input("Įveskite GKS: ")
-            gliukoze = input("Įveskite gliukozė: ")
-            temp = input("Įveskite temp: ")
-            priskirtas_pacientas_id = int(input("Įveskite paciento ID: "))
-            neuro = Neurologija(gks=gks, gliukoze=gliukoze, temp=temp, priskirtas_pacientas_id=priskirtas_pacientas_id)
-            session.add(neuro)
-            session.commit()
+        gks = input("Įveskite GKS: ")
+        gliukoze = input("Įveskite gliukozė: ")
+        temp = input("Įveskite temp: ")
+        priskirtas_pacientas_id = int(input("Įveskite paciento ID: "))
+        neuro = Neurologija(gks=gks, gliukoze=gliukoze, temp=temp, priskirtas_pacientas_id=priskirtas_pacientas_id)
+        session.add(neuro)
+        session.commit()
+    if pasirinkimas == 5:
+        pavadinimas = input("Įveskite ligoninės pavadinima: ")
+        adresas = input("Įveskite ligoninės adresą: ")
+        atvykimo_laikas = input("Įveskite paciento atvykimo laiką: ")
+        priskirtas_pacientas_id = int(input("Įveskite paciento ID: "))
+        ligonines = Ligonine(pavadinimas=pavadinimas, adresas=adresas, atvykimo_laikas=atvykimo_laikas, priskirtas_pacientas_id=priskirtas_pacientas_id)
+        session.add(ligonines)
+        session.commit()
 
     if pasirinkimas == 7:
         kvepuota = session.query(Kvepavimas).all()
         zmogus = session.query(Pacientas).all()
         kraujas = session.query(Kraujotaka).all()
         neuros = session.query(Neurologija).all()
+        ligonin = session.query(Ligonine).all()
         for zmones in zmogus:
-            print(f"Vardas:{vardas}, Pavardė:{pavarde}, Asmens kodas:{asmens_kodas}")
+            print(zmones)
         for deguonis in kvepuota:
-            print(f"SpO2:{spo2}%, KD:{kd}")
+            print(deguonis)
         for krauja in kraujas:
-            print(f"ŠSD:{pulsas}, AKS:{aks}, EKG:{ekg}")
+            print(krauja)
         for madula in neuros:
-            print(f"GKS:{gks}, Gliukozė:{gliukoze}, Temp:{temp}")
+            print(madula)
+        for istaiga in ligonin:
+            print(istaiga)
     if pasirinkimas == 0:
         break
     
--- paciento_info2.py
+++ paciento_info2.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey, create_engine, Table
+from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
@@ -12,6 +12,9 @@ class Pacientas(Base):
     vardas = Column("vardas", String)
     pavarde = Column("pavarde", String)
     asmens_kodas = Column("asmens kodas", Integer, unique=True)
+
+    def __repr__(self):
+        return f"{self.id}, {self.vardas}, {self.pavarde}, {self.asmens_kodas}"
 
 class Kvepavimas(Base):
     __tablename__ = "Kvepavimas"
@@ -45,6 +48,17 @@ class Neurologija(Base):
 
     def __repr__(self):
         return f"{self.id}, {self.gks}, {self.gliukoze}, {self.temp}"
+
+class Ligonine(Base):
+    __tablename__ = "Ligonine"
+    id = Column(Integer, primary_key=True)
+    pavadinimas = Column("pavadinimas", String)
+    adresas = Column("adresas", String)
+    atvykimo_laikas = Column("atvykimo laikas", Integer)
+    priskirtas_pacientas_id = Column(Integer, ForeignKey('pacientas.id'))
+
+    def __repr__(self):
+        return f"{self.id}, {self.pavadinimas}, {self.adresas}, {self.atvykimo_laikas}, {self.priskirtas_pacientas_id}"
 
 if __name__ == "__main__":
     Base.metadata.create_all(engine)
